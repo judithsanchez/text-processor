927dc4cc6ff3b537b9cbd138a0dda9c63964a0f7
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-06-27 12:14:27 -0500

    Created a class that takes a text and separates it into sentences and tokens

--- text-processor.js
+++ text-processor.js
@@ -1,13 +1,8 @@
 const sampleText =
   '¿Lorem ñipsum dólor sit amet?, ¡consectetur! adipiscing elit. Mauris consequat ullamcorper metus, vitae pellentesque nisl eleifend a. Suspendisse finibus dapibus leo at tincidunt. Etiam nec mi vel leo venenatis consectetur. Curabitur eu consectetur sapien, id lobortis tellus. Vivamus consequat, tortor a facilisis pellentesque, turpis ligula faucibus mauris, sit amet commodo urna ligula id sapien. Maecenas facilisis rutrum dapibus. In vel lacinia velit. Nulla facilisi. Quisque et dapibus lectus. Curabitur eu justo et nisi eleifend luctus. Proin vitae fermentum erat. Suspendisse potenti. Morbi dignissim metus et magna pellentesque, id aliquet velit sollicitudin. Mauris pharetra nunc eget ligula malesuada gravida. Nulla facilisis iaculis odio in egestas. Suspendisse non leo dui. Mauris sit amet dapibus ante. Vestibulum fringilla tristique mattis. Suspendisse laoreet, enim eu tincidunt aliquet, augue turpis lacinia justo, eget tempor dolor ipsum nec mauris. Ut rutrum, ipsum ut cursus ullamcorper, nunc orci interdum massa, vitae tincidunt odio justo in tellus. Praesent sed interdum neque. Integer congue libero a metus consequat tincidunt. Nam consectetur erat eu convallis interdum. Curabitur sit amet nunc sit amet nisl ullamcorper fermentum vel sed erat.';
 
-const separateIntoSentences = (text) => {
-  const sentences = text.split(/(?<=\.)\s*/);
-  return sentences;
-};
-
-const separateParagraphsIntoWords = (sentences) => {
-  const punctuationSigns = [
+class TextProcessor {
+  punctuationSigns = [
     ',',
     '.',
     '¡',
@@ -19,84 +14,103 @@ const separateParagraphsIntoWords = (sentences) => {
     "'",
     '(',
     ')',
+    '-',
+    '_',
+    '[',
+    ']',
+    '{',
+    '}',
+    '/',
+    '\\',
+    '|',
+    '@',
+    '#',
+    '$',
+    '%',
+    '&',
+    '*',
+    '+',
+    '=',
+    '<',
+    '>',
+    '~',
+    '`',
+    '"',
   ];
 
-  const result = [];
+  constructor(text) {
+    this.processedText = this.processText(text);
+  }
 
-  for (let sentence of sentences) {
-    const finalSentence = [];
-    let currentWord = '';
+  processText(text) {
+    const sentences = this.tokenizeSentences(text);
+    const result = sentences.map((sentence, index) => ({
+      sentenceId: index,
+      tokens: this.tokenizeSentence(sentence),
+    }));
+    return result;
+  }
 
-    for (let i = 0; i < sentence.length; i++) {
-      const currentChar = sentence[i];
+  tokenizeSentences(text) {
+    return text.split(/(?<=\.)\s*/);
+  }
 
-      if (currentChar === ' ') {
-        if (currentWord.length > 0) {
-          finalSentence.push(currentWord);
-          currentWord = '';
+  tokenizeSentence(sentence) {
+    const words = sentence.split(' ');
+    const tokenizedSentence = [];
+    let tokenId = 0;
+
+    for (const word of words) {
+      const tokens = this.tokenizeWord(word);
+
+      for (const token of tokens) {
+        const isPunctuationSign = this.isPunctuation(token);
+
+        const tokenObject = {
+          tokenId,
+          token,
+          isPunctuationSign,
+        };
+
+        if (!isPunctuationSign) {
+          tokenObject.languages = { spanish: '', english: '' };
         }
+
+        tokenizedSentence.push(tokenObject);
+        tokenId++;
+      }
+    }
+
+    return tokenizedSentence;
+  }
+
+  tokenizeWord(word) {
+    const tokenizedWord = [];
+    let currentToken = '';
+
+    for (const char of word) {
+      if (this.isPunctuation(char)) {
+        if (currentToken.length > 0) {
+          tokenizedWord.push(currentToken);
+          currentToken = '';
+        }
+        tokenizedWord.push(char);
       } else {
-        const isPunctuation = punctuationSigns.includes(currentChar);
-
-        if (isPunctuation && currentWord.length > 0) {
-          finalSentence.push(currentWord);
-          currentWord = '';
-        }
-
-        currentWord += currentChar;
-
-        if (isPunctuation) {
-          finalSentence.push(currentChar);
-          currentWord = '';
-        }
+        currentToken += char;
       }
     }
 
-    if (currentWord.length > 0) {
-      finalSentence.push(currentWord);
+    if (currentToken.length > 0) {
+      tokenizedWord.push(currentToken);
     }
 
-    result.push(finalSentence);
+    return tokenizedWord;
   }
 
-  return result;
-};
-
-const paragraphsWithSpaces = (paragraphs) => {
-  const openingPunctuationSigns = ['¡', '¿', '('];
-  const punctuationSigns = [',', '.', '!', '?', ':', ';', "'", ')'];
-
-  const result = [];
-
-  for (let i = 0; i < paragraphs.length; i += 1) {
-    const currentSentence = paragraphs[i];
-    const modifiedSentence = [];
-
-    for (let k = 0; k < currentSentence.length; k += 1) {
-      const currentWord = currentSentence[k];
-      const nextWord = currentSentence[k + 1];
-
-      modifiedSentence.push(currentWord);
-
-      if (!openingPunctuationSigns.includes(currentWord)) {
-        if (nextWord && !punctuationSigns.includes(nextWord)) {
-          modifiedSentence.push('');
-        }
-      }
-    }
-
-    result.push(modifiedSentence);
+  isPunctuation(char) {
+    return this.punctuationSigns.includes(char);
   }
+}
 
-  return result;
-};
-
-const mySeparatedText = separateIntoSentences(sampleText);
-const mySeparateParagraphsIntoWords =
-  separateParagraphsIntoWords(mySeparatedText);
-
-const myParagraphsWithSpaces = paragraphsWithSpaces(
-  mySeparateParagraphsIntoWords
-);
-
-console.log(myParagraphsWithSpaces);
+const newText = new TextProcessor(sampleText);
+console.log(newText);
